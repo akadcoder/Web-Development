72bf678b184d5d368c5144d6c99137b4e913d83e
shivraj <209shivrajsingh@gmail.com>
2025-07-09 15:58:51 +0530

    Day 18

---
+++ JS/JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,42 @@
+console.log("Hey this is day 21");
+
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "Marc";
+let _a = "Steven";
+// var 55a = "Specter"; // Not Allowed
+
+// console.log(a +  b + 8);
+// console.log(typeof a, typeof b, typeof c);
+{
+    // let a = 66;
+    console.log(a)
+}
+console.log(a)
+// const a1 = 6;
+// a1 = a1 + 1; // Not Allowed because a1 is constant
+
+
+let x = "Marc Specter";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    "name": "Marc",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
